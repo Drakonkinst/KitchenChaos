6c530d7fb06a65876a18748176fafcbe4369b19f
Drakonkinst <drakonkinstmc@gmail.com>
2023-07-20 05:24:05 -0700

    Add movement animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float turnSpeed = 10f;
+
+    private bool isWalking = false;
 
     private void Update() {
         // Get input vector
@@ -25,6 +28,15 @@ public class Player : MonoBehaviour
 
         // Move character
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * Time.deltaTime * moveSpeed;
+
+        // Rotate character to direction of movement
+        // Use Slerp for rotation, just Lerp for movement
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * turnSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
