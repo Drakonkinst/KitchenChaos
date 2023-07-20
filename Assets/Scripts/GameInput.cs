4f69b9c335d60e8a47dae9af2e326a6322f58066
Drakonkinst <drakonkinstmc@gmail.com>
2023-07-20 05:57:45 -0700

    Refactor input system

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        return inputVector.normalized;
+    }
+}
