d173150dd9c0a52dfdf4e23a8f1bc73bc08fea8a
Drakonkinst <drakonkinstmc@gmail.com>
2023-07-29 13:04:24 -0700

    Add plate UI

---
+++ Assets/Scripts/PlateIconsUI.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateIconsUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake() {
+        // Not making a prefab out of this to reduce prefab clutter, though it is a valid option
+        iconTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start() {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e) {
+        UpdateVisuals();
+    }
+
+    private void UpdateVisuals() {
+        // Clean up previous icons
+        foreach (Transform child in transform) {
+            // Do not destroy the original
+            if (child == iconTemplate) {
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
+
+        // Spawn icons
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()) {
+            Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+        }
+    }
+}
